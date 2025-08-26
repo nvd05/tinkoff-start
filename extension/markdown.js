0ffe132242d54d9f1761b0b9ca4c023cb2ce5781
nvd05 <nvd05@bk.ru>
2025-08-26 23:26:13 +0500

    Update markdown.js

--- extension/markdown.js
+++ extension/markdown.js
@@ -20,7 +20,7 @@ function getMarkdown (sections)
 		lines.push('');
 	}
 
-	for (const section of ['Вопрос', 'Формат входных данных', 'Формат выходных данных', 'Примечания', 'Примеры данных'])
+	for (const section of ['Вопрос', 'Формат входных данных', 'Формат выходных данных', 'Примечания'])
 	{
 		const data = sections[section]?.trim();
 
@@ -33,6 +33,50 @@ function getMarkdown (sections)
 		lines.push('');
 		lines.push(data);
 		lines.push('');
+	}
+
+	const examples = sections['Примеры данных']?.trim();
+	if (typeof examples === 'string')
+	{
+		lines.push('## Примеры данных');
+		lines.push('');
+
+		const array = [...examples.matchAll(/(Пример(\s*)(\d*)(\s*))?Ввод(\s*)([-\d\s\n]*)(\s*)Вывод(\s*)([-\d\s\n]*)/g)];
+
+		if (array.length === 0)
+		{
+			lines.push('> не удалось обработать по шаблону.');
+			lines.push('');
+			lines.push(examples);
+			lines.push('');
+		}
+
+		for (const example of array)
+		{
+			const title = example[3]?.trim();
+			const input = example[6]?.trim();
+			const output = example[9]?.trim();
+
+			if (title)
+			{
+				lines.push('### Пример ' + title);
+				lines.push('');
+			}
+
+			lines.push('Ввод:');
+			lines.push('');
+			lines.push('```text');
+			lines.push(input);
+			lines.push('```');
+			lines.push('');
+
+			lines.push('Вывод:');
+			lines.push('');
+			lines.push('```text');
+			lines.push(output);
+			lines.push('```');
+			lines.push('');
+		}
 	}
 
 	return lines.join('\n');
