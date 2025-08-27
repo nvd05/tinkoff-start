054002437a625ddf4bdec8865820f53bd2d1e59d
nvd05 <nvd05@bk.ru>
2025-08-27 23:38:02 +0500

    добавлены ключи для разделов в скрипте

--- extension/sections.js
+++ extension/sections.js
@@ -1,56 +1,72 @@
 
+/** @typedef { 'task' | 'points' | 'time_limit' | 'memory_limit' } TinkoffHeaderKeys */
+/** @typedef { 'question' | 'input_format' | 'output_format' | 'notes' | 'examples' } TinkoffContentKeys */
+/** @typedef { 'solution' | 'history' } TinkoffFooterKeys */
+/** @typedef { 'unprocessed' | TinkoffHeaderKeys | TinkoffContentKeys | TinkoffFooterKeys } TinkoffSectionKeys */
+
+/** @type { { regex: RegExp, title: string, key: TinkoffSectionKeys }[] } */
 const sections = [
 	{
 		regex: /Предыдущие решения(.*)$/s,
 		title: 'Предыдущие решения',
+		key: 'history',
 	},
 	{
 		regex: /Решение(.*)$/s,
 		title: 'Решение',
+		key: 'solution',
 	},
 	{
 		regex: /Примеры данных(.*)$/s,
 		title: 'Примеры данных',
+		key: 'examples',
 	},
 	{
 		regex: /Замечани[е|я](.*)$/s,
 		title: 'Примечания',
+		key: 'notes',
 	},
 	{
 		regex: /Формат выходных данных(.*)$/s,
 		title: 'Формат выходных данных',
+		key: 'output_format',
 	},
 	{
 		regex: /Формат входных данных(.*)$/s,
 		title: 'Формат входных данных',
+		key: 'input_format',
 	},
 	{
 		regex: /МБ(.*)$/s,
 		title: 'Вопрос',
+		key: 'question',
 	},
 	{
 		regex: /Ограничение памяти\s*(\d*)/,
 		title: 'Ограничение памяти',
+		key: 'memory_limit',
 	},
 	{
 		regex: /Ограничение времени\s*(\d*\sсекунда)/,
 		title: 'Ограничение времени',
+		key: 'time_limit',
 	},
 	{
 		regex: /(\d*)\s*баллов/,
 		title: 'Балл',
+		key: 'points',
 	},
 	{
 		regex: /(\d*)\s*задание/,
 		title: 'Задание',
+		key: 'task',
 	},
 ];
 
 function getSections (text)
 {
-	const responses = {
-		unprocessed: null
-	};
+	/** @type { Partial<Record<TinkoffSectionKeys, Record<'title' | 'value', string>>> } */
+	const responses = {};
 
 	for (const section of sections)
 	{
@@ -61,11 +77,21 @@ function getSections (text)
 			continue;
 		}
 
-		responses[section.title] = response[1].trim();
+		responses[section.key] = {
+			title: section.title,
+			value: response[1].trim(),
+		};
+
 		text = text.substring(0, response.index).trim();
 	}
 
-	responses.unprocessed = text;
+	if (text)
+	{
+		responses.unprocessed = {
+			title: 'Необработанный текст',
+			value: text,
+		};
+	}
 
 	return responses;
 }
